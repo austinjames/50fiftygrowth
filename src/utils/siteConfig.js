f34ab3a253b4b3cc0ecc9f159f5ce3498ac8564b
Austin James <maustinjames@gmail.com>
2020-03-01 02:26:00 -0800

    Edited site config

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Ghost Gatsby Starter`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `50/fifty Growth`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `Coming Soon`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `50/fiftyGrowth`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
